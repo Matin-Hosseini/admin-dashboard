99a4171e20b7ad88f2c5e24f777cb17a1352d1c9
Matin <matinhosseiniofficial@gmail.com>
2024-07-17 11:46:30 +0330

    customer dialog

---
+++ app/components/AddNewCustomer.jsx
@@ -0,0 +1,89 @@
+import { useState } from "react";
+import { RiUserAddLine } from "react-icons/ri";
+
+import {
+  Dialog,
+  Button,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  TextField,
+} from "@mui/material";
+import { useForm } from "react-hook-form";
+
+const AddNewCustomer = () => {
+  const [showDialog, setshowDialog] = useState(false);
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
+
+  const submitHandler = async (data) => {
+    console.log(data);
+  };
+
+  return (
+    <>
+      <Button
+        startIcon={<RiUserAddLine />}
+        className="mb-5"
+        onClick={() => setshowDialog(true)}
+      >
+        افزودن مشتری جدید
+      </Button>
+      <Dialog
+        sx={{ "& .MuiPaper-root": { width: "500px" } }}
+        open={showDialog}
+        onClose={() => setshowDialog(false)}
+      >
+        <DialogTitle>افزودن مشتری جدید</DialogTitle>
+        <DialogContent>
+          <form action="#" onSubmit={handleSubmit(submitHandler)}>
+            <TextField
+              className="mb-2 w-full"
+              label="نام"
+              variant="standard"
+              {...register("firstName")}
+            />
+            <TextField
+              className="mb-2 w-full"
+              label="نام خانوادگی"
+              variant="standard"
+              {...register("lastName")}
+            />
+            <TextField
+              className="mb-2 w-full"
+              label="شماره موبایل"
+              variant="standard"
+              {...register("nationalCode")}
+            />
+            <TextField
+              className="mb-2 w-full"
+              label="کد ملی"
+              variant="standard"
+              {...register("phoneNumber")}
+            />
+
+            <div className="mt-3 flex items-center justify-end gap-2">
+              <Button
+                color="error"
+                variant="outlined"
+                onClick={() => setshowDialog(false)}
+              >
+                لغو
+              </Button>
+              <Button color="primary" variant="contained" type="submit">
+                ثبت
+              </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+    </>
+  );
+};
+
+export default AddNewCustomer;
