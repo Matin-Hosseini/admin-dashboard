d00b582e483bb9b86c943e5610ac19d85af08ee9
Matin <matinhosseiniofficial@gmail.com>
2024-07-18 12:58:29 +0330

    page

--- app/page.js
+++ app/page.js
@@ -1,14 +1,44 @@
 "use client";
 import Sidebar from "./components/Sidebar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "@/app/components/Header";
 import Table from "./components/Table";
-import { Button } from "@mui/material";
-import { RiUserAddLine } from "react-icons/ri";
 import AddNewCustomer from "./components/AddNewCustomer";
+import Cookies from "js-cookie";
+import { useRouter } from "next/navigation";
+import axios from "axios";
 
 export default function Home() {
   const [isSidbarOpen, setIsSidebarOpen] = useState(false);
+  const [customers, setCustomers] = useState([]);
+
+  const router = useRouter();
+  console.log(router);
+
+  const token = Cookies.get("token");
+  if (!token) {
+    router.push("/login");
+  }
+
+  useEffect(() => {
+    const getUsers = async () => {
+      const res = await axios.post(
+        "https://iroriginaltest.com/api/Customer/GetAllCustomers",
+        {
+          typeOfRegisterCustomer_Id: null,
+          registerDate: null,
+          phoneNumber: null,
+          nationalCode: null,
+          token: Cookies.get("token"),
+        }
+      );
+
+      console.log(res.data);
+      setCustomers(res.data.result.informationCustomer);
+    };
+
+    getUsers();
+  }, []);
 
   return (
     <>
@@ -23,7 +53,7 @@ export default function Home() {
           <main className="main">
             <div className="content">
               <AddNewCustomer />
-              <Table />
+              <Table customers={customers} />
             </div>
           </main>
         </div>
